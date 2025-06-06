a5a7ca13e7d4bb6776b0e37d1c1d2664b7d21a5c
yishain11 <yishain11@walla.co.il>
2025-06-06 09:51:53 +0300

    Add project files.

---
+++ NoExceptions/NoException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorsAndExaceptions.NoExceptions
+{
+    internal class NoException
+    {
+        public void NoExecptionCode() {
+            // syntax err
+            //Console.WriteLine("he there";
+
+
+            // err 1
+            string name = "yishai";
+            int nameInt = int.Parse(name);
+            Console.WriteLine($"int name is: {nameInt}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ErrorsAndExaceptions.NoExceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorsAndExaceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NoException nc = new NoException();
+            nc.NoExecptionCode();
+        }
+    }
+}
